d88d0fa7fa152fe2dcc579d0a5a96e1781a72b19
tristansimonson <tristan-simonson@uiowa.edu>
2019-04-26 10:21:52 -0500

    added more hw6 files main, checker, and interp to start testing parser

---
+++ PlcChecker.fs
@@ -0,0 +1,54 @@
+
+module PlcChecker
+
+open Absyn
+open Environ
+
+// The type checker can be seen as an interpreter that computes
+// the type of an expression instead of its value.
+
+let rec teval (e : expr) (env : plcType env) : plcType =
+    match e with
+    | List [] -> ListT []
+    | List es -> ListT (List.map (fun e -> teval e env) es)
+    | CstI i -> IntT
+    | CstB b -> BoolT
+    | Var x -> lookup env x
+    | Let(x, e1, letBody) -> let xTyp = teval e1 env 
+                             let letBodyEnv = (x, xTyp) :: env
+                             teval letBody letBodyEnv  
+    | Prim1 (op, e1) -> let i1 = teval e1 env in
+                          match op with
+                          | "-" -> if (i1 = IntT) then (IntT; IntT) else failwith ("Prim1: cannot use negation on type " + (type2string i1))
+                          | "!" -> if (i1 = BoolT) then (BoolT; BoolT) else failwith ("Prim1: cannot use not on type " + (type2string i1))
+                          | _ -> failwith ("Prim1: undefined unary operator " + op)
+    | Prim2 (op, e1, e2) -> let i1 = teval e1 env in
+                              let i2 = teval e2 env in
+                                match op with
+                                | "*" -> if (i1 = IntT && i1 = i2) then (IntT; IntT; IntT) else failwith ("Prim2: cannot multiply non-int types " + (type2string i1) + " and " + (type2string i2))
+                                | "+" -> if (i1 = IntT && i1 = i2) then (IntT; IntT; IntT) else failwith ("Prim2: cannot add non-int types " + (type2string i1) + " and " + (type2string i2))
+                                | "-" -> if (i1 = IntT && i1 = i2) then (IntT; IntT; IntT) else failwith ("Prim2: cannot subtract non-int types " + (type2string i1) + " and " + (type2string i2))
+                                | "=" -> if (i1 = i2) then (i1; i2; BoolT) else failwith ("Prim2: mismatching equivalence return types " + (type2string i1) + " and " + (type2string i2))
+                                | "<" -> if (i1 = IntT && i1 = i2) then (IntT; IntT; BoolT) else failwith ("Prim2: cannot LT compare non-int types " + (type2string i1) + " and " + (type2string i2))
+                                | _   -> failwith ("Prim2: undefined binary operator " + op)
+    | Letfun(f, x, xTyp, fBody, rTyp, letBody) -> let fTyp = FunT(xTyp, rTyp)
+                                                  let fBodyEnv = (x, xTyp) :: (f, fTyp) :: env
+                                                  let letBodyEnv = (f, fTyp) :: env
+                                                  if teval fBody fBodyEnv = rTyp
+                                                    then teval letBody letBodyEnv
+                                                  else failwith ("Letfun: return type in " + f + " of " + type2string rTyp + " does not match " + type2string (teval fBody fBodyEnv))   
+    | If (e1, e2, e3) -> match (teval e1 env), (teval e2 env), (teval e3 env) with
+                         | BoolT, t2, t3 when (t2 = t3) -> t2
+                         | BoolT, t2, t3 when (t2 <> t3) -> failwith ("If: different return types " + (type2string t2) + ", " + (type2string t3))
+                         | t1, t2, t3 -> failwith ("If: first expression not bool: " + (type2string t1))
+    | Call(Var f, eArg) -> match lookup env f with
+                           | FunT(xTyp, rTyp) -> if teval eArg env = xTyp 
+                                                   then rTyp 
+                                                 else failwith ("Call: wrong argument type " + type2string (teval eArg env))
+                           | _ -> failwith ("Call: unknown function " + type2string (lookup env f))
+    | Item (n, e1) -> match teval e1 env with
+                      | ListT vs -> try 
+                                      List.item (n - 1) vs
+                                    with
+                                      | :? System.ArgumentException -> failwith ("Item: index out of range with position " + string n)
+                      | _ -> failwith ("Item: not a list type " + type2string (teval e1 env))
---
+++ PlcInterp.fs
@@ -0,0 +1,70 @@
+
+
+module PlcInterp
+
+open Absyn
+open Environ
+
+let rec eval (e : expr) (env : plcVal env) : plcVal =
+    match e with
+    | CstI i -> IntV i
+    | CstB b -> BoolV b
+
+    | Var x  ->
+      let v = lookup env x in
+      match v with
+      | IntV  _ -> v
+      | BoolV _ -> v
+      | ListV  _ -> v
+      | _      -> failwith ("Value of variable _" + x + "_ is not first-order.")
+
+    | Prim1 (op, e1) ->
+      let v1 = eval e1 env in
+      match (op, v1) with
+      | ("-", IntV i) -> IntV (- i)
+      | ("!", BoolV b) -> BoolV (not b)
+      | _   -> failwith "Impossible"
+
+    | Prim2 (op, e1, e2) ->
+      let v1 = eval e1 env in
+      let v2 = eval e2 env in
+      match (op, v1, v2) with
+      | ("=", _, _) -> BoolV (v1 = v2)
+      | ("<", IntV i1, IntV i2) -> BoolV (i1 < i2)
+      | ("*", IntV i1, IntV i2) -> IntV (i1 * i2)
+      | ("+", IntV i1, IntV i2) -> IntV (i1 + i2)
+      | ("-", IntV i1, IntV i2) -> IntV (i1 - i2)
+      | _   -> failwith "Impossible"
+
+    | Let (x, e1, e2) ->
+      let v = eval e1 env in
+      let env2 = (x, v) :: env in
+      eval e2 env2
+
+    | If (e1, e2, e3) ->
+      let v1 = eval e1 env in
+      match v1 with
+      | BoolV true  -> eval e2 env
+      | BoolV false -> eval e3 env
+      | _ -> failwith "Impossible"
+
+    | Letfun (f, x, _, e1, _, e2) ->
+      let env2 = (f, Closure(f, x, e1, env)) :: env in
+      eval e2 env2
+
+    | Call (Var f, e) ->
+      let c = lookup env f in
+      match c with
+      | Closure (f, x, e1, fenv) ->
+        let v = eval e env in
+        let env1 = (x, v) :: (f, c) :: fenv in
+        eval e1 env1
+      | _ -> failwith "eval Call: not a function"
+    | Call _ -> failwith "eval Call: not first-order function"
+
+    | List es -> ListV (List.map (fun e -> eval e env) es)
+
+    | Item (n, e1) ->
+      match eval e1 env with
+      | ListV vs -> List.item (n - 1) vs
+      | _ -> failwith "Impossible"
