c92c5ffbf0ddd9cb76f88469fd9641402177c84d
Tejas M <36649349+TejM@users.noreply.github.com>
2019-04-28 14:42:52 -0500

    Update PlcParserAux.fs

--- PlcParserAux.fs
+++ PlcParserAux.fs
@@ -6,10 +6,18 @@ let l =  "$list"
 let elist =  "()"
 
 let rec makeFunAux (n: int) (xs: (plcType * string) list) (e: expr) : expr =
-    e // TODO
+  match xs with
+  |[(t,x)] -> Let (x, Item(n, Var (l)), e)
+  | (t,x) :: tail -> Let (x, Item(n, Var (l)), (makeFunAux (n+1) (tail) (e)))
+    
+let rec makeTypeHelper (l: (plcType * string) list): plcType list =
+  match l with
+  | [(t, x)] -> [t] 
+  | (t,x) :: tail -> [t] @ makeTypeHelper tail
 
-let makeType (args: (plcType * string) list): plcType =
-    ListT [] // TODO
+let makeType (l: (plcType * string) list): plcType =
+   ListT (makeTypeHelper (l))
+
 
 let makeFun (f: string) (xs: (plcType * string) list) (rt: plcType) (e1: expr) (e2: expr) : expr =
     match xs with
